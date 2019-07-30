061c24d161d0a783b3ab99a845a1e2836796e42a
shetty-Pranita22 <pranitashetty172@yahoo.com>
2019-07-30 14:43:28 +0530

    collections pages done with routing

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,19 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import logger from "redux-logger";
+import { persistStore } from "redux-persist";
 
 import rootReducer from "./root-reducer";
 
 const middlewares = [logger];
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
+export const store = createStore(
+  rootReducer,
+  compose(
+    applyMiddleware(...middlewares)
+    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
 
-export default store;
+export const persistor = persistStore(store);
+
+export default { store, persistor };
